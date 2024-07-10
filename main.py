8430615164b5e9a0f8943a4c82fffd9dbf4e7465
nothing <161198246+PYHOMES00@users.noreply.github.com>
2024-07-10 23:15:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import config
+import time
+import logging
+from pyrogram import Client, idle
+from pyromod import listen  # type: ignore
+from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+
+StartTime = time.time()
+app = Client(
+    "DAXX",
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+    in_memory=True,
+    plugins=dict(root="codewithshubham"),
+)
+
+
+if __name__ == "__main__":
+    print("ꜱᴛᴀʀᴛɪɴɢ ʏᴏᴜʀ ꜱᴛʀɪɴɢ ʙᴏᴛ ...")
+    try:
+        app.start()
+    except (ApiIdInvalid, ApiIdPublishedFlood):
+        raise Exception("Your API_ID/API_HASH is not valid.")
+    except AccessTokenInvalid:
+        raise Exception("Your BOT_TOKEN is not valid.")
+    uname = app.get_me().username
+    print(f"@{uname} ꜱᴛᴀʀᴛᴇᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ. ᴍᴀᴅᴇ ʙʏ @The_creator_Botz 🤗")
+    idle()
+    app.stop()
+    print("ʙᴏᴛ ꜱᴛᴏᴘᴘᴇᴅ!")
